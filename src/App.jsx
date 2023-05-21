bbb3431576f5778439239ffea50e1cdce07cb868
trippyyy333 <75495653+trippyyy333@users.noreply.github.com>
2023-05-20 23:19:34 -0400

    setup gh-pages45jjh

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter } from "react-router-dom";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+    <div className="relative z-0 bg-primary">
+      <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+
+        cas
+      </div>
+    </div>
+
+    
+      <div>dhsjk</div><nav><ul><li>f</li>
+      <li>ss</li>
+      <li>s</li>
+      <li>sa</li>
+      <li>ds</li></ul></nav>
+      </BrowserRouter>
+
+  );
+}
+
+export default App;
